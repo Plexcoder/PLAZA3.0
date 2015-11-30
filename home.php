a6126ada2bff8cde83e368b35a98aa43122de4db
Gijs Wiggers <gbwiggers@gmail.com>
2015-11-30 13:18:36 +0100

    Update to plaza
    It isn't a grade system anymore. IT'S MUCH MORE!

--- home.php
+++ home.php
@@ -20,13 +20,13 @@ $result = mysql_query("SELECT * FROM members");
 	<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
 			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-			<title>Partner care grade system Welcome</title>
+			<title>PLAZA 3.0 // Welcome</title>
 			<link rel="stylesheet" href="style.css" type="text/css" />
 		</head>
 		<body>
 			<div id="header">
 			  <div id="left">
-				<label>grade assessment exchange</label>
+				<label>Plaza 3.0</label>
 			  </div>
 			  <div id="right">
 				<div id="content">
